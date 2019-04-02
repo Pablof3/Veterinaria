0039dad33e9dad79579b806fa930006cbe2ae58f
Mauritius Gamarra <mauriciogamarravargas@gmail.com>
2019-04-02 16:50:52 -0400

    vistas registrar cargo y categoria

--- app/controllers/Categoria.php
+++ app/controllers/Categoria.php
@@ -22,7 +22,12 @@ class Categoria extends Controller
     public function Eliminar()
     {
      
-        print_r( $this->mCategoria->Eliminar(2));  
+        $this->mCategoria->Eliminar();  
+    }
+
+    public function vRegistrar()
+    {
+        $this->vista('Categoria/vRegistrar');
     }
 }
 
---
+++ app/views/Cargo/vRegistrar.php
@@ -0,0 +1,32 @@
+<?php require RUTA_APP .'/views/inc/Header.php';?>
+<div class=" card mb-4">
+    <div class="card-body">
+        <h5 class="mb-3">Registro Cargo</h5>
+        <form class="needs-validation mb-5" novalidate method="POST" action="<?= RUTA_URL?>/Cargo/Registrar">
+            <div class="form-row">
+                <div class="col-md-4 mb-3">
+                    <label for="validationTooltip001">Nombre de cargo</label>
+                    <input type="text" class="form-control" 
+                        id="validationTooltip001" 
+                        placeholder="Cargo"
+                        name="Cargo[nombre]" 
+                        maxlength="30" 
+                        required>
+                    <div class="invalid-tooltip">
+                        Campo Requerido
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">    
+                </div>
+            </div>
+            <button class="btn btn-primary" type="submit">
+                Registrar
+            </button>
+        </form>
+    </div>
+</div>
+<?php require RUTA_APP .'/views/inc/Footer.php';?>
+<?php
+    $libs=[0=>'Cargo'];
+    InsertLib($libs);
+?>

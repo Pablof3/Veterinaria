aa94b4aaf8c9edbd0def3a81dc9e42474eeb77d4
Mauritius Gamarra <mauriciogamarravargas@gmail.com>
2019-04-05 10:00:16 -0400

    cambios controller imagen

--- app/controllers/Imagen.php
+++ app/controllers/Imagen.php
@@ -9,16 +9,16 @@ class Imagen extends Controller
     public function Registrar()
     {
         $imagen=new Core\Imagen;
-        $imagen->id_Propietario=1;
-        $imagen->path='ubicacion';
+        $imagen->id_Propietario=$_POST['Imagen']['id_Propietario'];
+        $imagen->path=$_POST['Imagen']['path'];
         $this->mImagen->Insertar($imagen);  
     }
     public function Actualizar()
     {
         $imagen=new Core\Imagen;
-        $imagen->id_Imagen=1;
-        $imagen->id_Propietario=1;
-        $imagen->path='ubicacion1';
+        $imagen->id_Imagen=$_POST['Imagen']['id_Imagen'];
+        $imagen->id_Propietario=$_POST['Imagen']['id_Propietario'];
+        $imagen->path=$_POST['Imagen']['path'];
         $this->mImagen->Actualizar($imagen);   
     }
     public function Eliminar()
